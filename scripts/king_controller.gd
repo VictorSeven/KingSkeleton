d6bc4460603fef2d2b424b1c76e5f492b97ca263
VictorSeven <vbuendiar@gmail.com>
2017-11-10 19:40:56 +0100

    Movement and collisions tuned

--- scripts/king_controller.gd
+++ scripts/king_controller.gd
@@ -5,37 +5,67 @@ extends KinematicBody2D
 
 #TODO: draw something.
 
-var acel = 15.0 #Acceleration
+var acel = Vector2(0.5, 0.8) #Acceleration
 var vel = Vector2(0.0, 0.0) #Speed
-var dir = Vector2(0.0, 0.0) #Which direction I have pressed
-var max_speed = 3.0 #Maximum allowed speed
-var sqbrake = 0.3 #Allows smooth brake
+var max_speed = Vector2(3.0, 1.5) #Maximum allowed speed
+var brake = 0.1 #Allows smooth brake
+var brakelim = brake*1.01 #Delete residual speed of brake
+
+var jump_height = 60 #Height you can jump
+var target_height = -1.0 #Where do I have to jump? 
+var start_height #Height where jump started
+
 
 func _ready():
+	start_height = get_pos().y #Initial start height
 	set_fixed_process(true) #Start the fixed process
 
 #Proceso fijo
 func _fixed_process(delta):
 	move_input(delta)
-	set_pos(get_pos() + vel)
+	if (is_colliding()):
+		var n = get_collision_normal()
+		vel = n.slide(vel)
+		
+	move(vel)
+	#set_pos(get_pos() + vel)
+
 
 #Controls the input in an abstract way
 func move_input(delta):
 	#TODO: allow diagonal movement 
 	if (Input.is_action_pressed("ui_right")):
-		vel.x += acel * delta
+		vel.x += acel.x
 	elif (Input.is_action_pressed("ui_left")):
-		vel.x -= acel * delta
-	elif (Input.is_action_pressed("ui_down")):
-		vel.y += acel * delta
-	elif (Input.is_action_pressed("ui_up")):
-		vel.y -= acel * delta
+		vel.x -= acel.x 
 	else:
-		vel += -vel / acel #Smooth brake
-		
+		vel.x += - sign(vel.x) * brake #Smooth brake
 		#Delete residual speed
-		if (vel.length_squared() < sqbrake):
-			vel = Vector2(0.0,0.0)
+		if (abs(vel.x) < brakelim):
+			vel.x = 0.0
 	
-	if (vel.length_squared() > max_speed * max_speed):
-		vel = vel.normalized() * max_speed
+	#Init jump
+	if (Input.is_action_pressed("ui_up")):
+		#If we have not started a jump, then start one
+		if (target_height == -1):
+			start_height = get_pos().y #Current height
+			target_height = start_height - jump_height #Get where I want to jump
+		#Once we have the target height, stop it
+		elif (get_pos().y < target_height):
+			vel.y = 0 
+		#If we still did not reach it -continue up
+		else:
+			vel.y = -max_speed.y #Up with constant speed
+	#Drop the button, then go down
+	else:
+		vel.y += acel.y #Simulate gravity
+		#Maximum speed of fall
+		if (vel.y > max_speed.y):
+			vel.y = max_speed.y
+		#If we got at the save height or more, then our jump has finised.
+		if (get_pos().y >= start_height):
+			target_height = -1.0
+	
+	
+	if (abs(vel.x) > max_speed.x):
+		vel.x = sign(vel.x) * max_speed.x
